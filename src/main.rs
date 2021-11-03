8db5ab3ad9a8bc1ba4bb973f768f3e75f6a362a7
Caio Vieira <caiovieiraprofissional@protonmail.com>
2021-11-03 19:06:34 -0300

    Update code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use rand;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let number = rand::random::<u8>();
+
+    println!("Eu pensei em um número entre 0 e 255,");
+    let guess = input("Qual você acha que é? ").expect("Erro ao ler entrada!");
+    let guess = guess
+        .parse::<u8>()
+        .expect("O valor inserido deve ser um número.");
+
+    if number == guess {
+        println!("Parabéns! Você acertou o número!");
+    } else {
+        println!("Opa! o número era {}, não {}.", number, guess);
+    }
+}
+
+fn input(msg: &str) -> Result<String, io::Error> {
+    print!("{}", msg);
+    io::stdout().flush()?;
+
+    let mut s = String::new();
+    io::stdin().read_line(&mut s)?;
+
+    Ok(s.trim().to_owned())
 }
